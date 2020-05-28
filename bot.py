37e3aa9f07da5de257804e3f03fa85edaac05256
Fido2603 <hero@herogamers.xyz>
2020-05-28 16:51:58 +0200

    Change to using Discord's own JSON formatting for embeds

--- bot.py
+++ bot.py
@@ -1,31 +1,9 @@
-from datetime import datetime
-
 import discord
 from discord import Embed
 from discord.ext import commands
-import os
 import json
 
 bot = commands.Bot(command_prefix="")
-
-
-class ConfigEmbed:
-    def __init__(self, channelid, title=None, description=None, timestamp=None, color=None, footertext=None,
-                 footericon=None, imageurl=None, thumbnailurl=None, authorname=None, authoricon=None, authorurl=None,
-                 fields=None):
-        self.channel = channelid
-        self.title = title
-        self.description = description
-        self.timestamp = timestamp
-        self.color = color
-        self.footertext = footertext
-        self.footericon = footericon
-        self.image = imageurl
-        self.thumbnail = thumbnailurl
-        self.authorname = authorname
-        self.authoricon = authoricon
-        self.authorurl = authorurl
-        self.fields = fields
 
 
 class DiscordEmbed:
@@ -37,136 +15,28 @@ class DiscordEmbed:
 # Embeds
 async def checkEmbeds():
     # Get embeds from the config
+    discord_embeds = []
+    channels = []
     with open('config.json', encoding='utf-8') as json_file:
         data = json.load(json_file)
 
-        embeds = []
-        for channel in data['Embeds']:
-            for e in data['Embeds'][channel]:
-                footertext, footerurl, authorname, authorimage, authorurl, title, color, desc, imageurl, timestamp, \
-                thumbnailurl = None, None, None, None, None, None, None, None, None, None, None
+        for configchannel in data['Embeds']:
+            for e in data['Embeds'][configchannel]:
+                channel = bot.get_channel(int(configchannel))
 
-                # Footer and author
-                if 'footer' in e:
-                    if 'text' in e['footer']:
-                        footertext = e['footer']['text']
-                    if 'icon_url' in e['footer']:
-                        footerurl = e['footer']['icon_url']
-                if 'author' in e:
-                    if 'name' in e['author']:
-                        authorname = e['author']['name']
-                    if 'image' in e['author']:
-                        authorimage = e['author']['image']
-                    if 'url' in e['author']:
-                        authorurl = e['author']['url']
+                if channel:
+                    channels.append(channel)
 
-                # Fields
-                fields = []
-                if 'fields' in e:
-                    for field in e['fields']:
-                        if ('title' in field) and ('content' in field):
-                            fieldtitle = field['title']
-                            fieldcontent = field['content']
-                            if not 'inline' in field:
-                                fieldinline = True
-                            else:
-                                fieldinline = field['inline']
-
-                            fields.append({"title": fieldtitle, "content": fieldcontent, "inline": fieldinline})
-
-                # Standard keys
-                if 'title' in e:
-                    title = e['title']
                 if 'color' in e:
-                    color = e['color'].replace("#", "")
-                if 'description' in e:
-                    desc = e['description']
-                if 'timestamp' in e:
-                    try:
-                        timestamp = datetime.strptime(e['timestamp'], "%Y-%m-%d %H:%M:%S")
-                    except ValueError:
-                        print("Couldn't get timestamp from embed. Timestamp '" + e['timestamp'] + "' doesn't match the "
-                              "format '%Y-%m-%d %H:%M:%S'")
-                if 'image' in e:
-                    imageurl = e['image']
-                if 'thumbnail' in e:
-                    thumbnailurl = e['thumbnail']
+                    e['color'] = int(e['color'].replace("#", ""), 16)
 
-                embed = ConfigEmbed(channelid=channel, title=title, color=color, description=desc,
-                                    imageurl=imageurl, thumbnailurl=thumbnailurl, authoricon=authorimage,
-                                    authorname=authorname, authorurl=authorurl, footertext=footertext,
-                                    footericon=footerurl, fields=fields, timestamp=timestamp)
+                try:
+                    embed = discord.Embed.from_dict(e)
+                except Exception as error:
+                    print(f"Error while getting embed from config - {error}")
+                    continue
 
-                embeds.append(embed)
-
-    # Send/check the embeds
-    discord_embeds = []
-    channels = []
-    for e in embeds:
-        channel = bot.get_channel(int(e.channel))
-
-        if channel:
-            channels.append(channel)
-
-            title = Embed.Empty
-            if e.title:
-                title = e.title
-            description = Embed.Empty
-            if e.description:
-                description = e.description
-            color = None
-            if e.color:
-                color = discord.Color(value=int(e.color, 16))
-            timestamp = None
-            if e.timestamp:
-                timestamp = e.timestamp
-
-            if color and timestamp:
-                embed = discord.Embed(title=title, description=description, color=color, timestamp=timestamp)
-            elif color:
-                embed = discord.Embed(title=title, description=description, color=color)
-            elif timestamp:
-                embed = discord.Embed(title=title, description=description, timestamp=timestamp)
-            else:
-                embed = discord.Embed(title=title, description=description)
-
-            # Footer
-            footertext = Embed.Empty
-            if e.footertext:
-                footertext = e.footertext
-            footericon = Embed.Empty
-            if e.footericon:
-                footericon = e.footericon
-
-            embed.set_footer(text=footertext, icon_url=footericon)
-
-            # Images
-            if e.image:
-                embed.set_image(url=e.image)
-
-            if e.thumbnail:
-                embed.set_thumbnail(url=e.thumbnail)
-
-            # Author
-            authorname = ""
-            if e.authorname:
-                authorname = e.authorname
-            authoricon = Embed.Empty
-            if e.authoricon:
-                authoricon = e.authoricon
-            authorurl = Embed.Empty
-            if e.authorurl:
-                authorurl = e.authorurl
-
-            embed.set_author(name=authorname, icon_url=authoricon, url=authorurl)
-
-            # Fields
-            if e.fields:
-                for field in e.fields:
-                    embed.add_field(name=field['title'], value=field['content'], inline=field['inline'])
-
-            # Channel
-            discord_embeds.append(DiscordEmbed(channel, embed))
+                discord_embeds.append(DiscordEmbed(channel, embed))
 
     # Check channels for the embeds
     channels = set(channels)
@@ -218,6 +88,8 @@ async def checkEmbeds():
                         print("timestamp difference")
                     elif current_embed.image.url != embed.image.url:
                         print("image difference")
+                    elif current_embed.video.url != embed.video.url:
+                        print("video difference")
                     elif current_embed.thumbnail.url != embed.thumbnail.url:
                         print("thumbnail difference")
                     elif (current_embed.footer.text != embed.footer.text) or (current_embed.footer.icon_url != embed.footer.icon_url):
@@ -246,9 +118,10 @@ async def checkEmbeds():
                                     found_difference = True
                                     break
                                 elif current_field.inline != field.inline:
-                                    print("fields difference - inline")
-                                    found_difference = True
-                                    break
+                                    if (current_field.inline is not False) and (field.inline is not Embed.Empty):
+                                        print("fields difference - inline")
+                                        found_difference = True
+                                        break
                                 field_i += 1
                             if not found_difference:
                                 print("No differences!")
